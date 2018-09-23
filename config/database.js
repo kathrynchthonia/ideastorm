b6fa959e520429fdfb1807fe447871374d69bab4
Your NameTony <tony@wheelertechconsulting.com>
2018-09-23 03:03:36 -0500

    updated mongo uri

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
-  module.exports = {mongoURI: "mongodb://<tony>:<tony4444>@ds111993.mlab.com:11993/ideastorm-prod"}
+  module.exports = {mongoURI: "mongodb://tony:tony4444@ds111993.mlab.com:11993/ideastorm-prod"}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/ideastorm-dev'}
 }
